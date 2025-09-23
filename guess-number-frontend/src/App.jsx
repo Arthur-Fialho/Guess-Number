68c1ca5d01949f83e5fc671a2fcfa93d0e9cc82d
Arthur-Fialho <arthursouza.fp@gmail.com>
2025-09-23 13:25:48 -0300

    feat: show attempts and guess history

--- guess-number-frontend/src/App.jsx
+++ guess-number-frontend/src/App.jsx
@@ -8,19 +8,23 @@ function App() {
   const [message, setMessage] = useState('Bem-vindo! Clique em "Novo Jogo" para começar.');
   const [isLoadingNewGame, setIsLoadingNewGame] = useState(false); // Estado para controlar o carregamento do novo jogo
   const [isLoadingGuess, setIsLoadingGuess] = useState(false); // Estado para controlar o carregamento do palpite
+  const [attempts, setAttempts] = useState(0);
+  const [guessHistory, setGuessHistory] = useState([]);
+  const [isGameOver, setIsGameOver] = useState(false);
 
   // Função para iniciar um novo jogo
   const handleStartGame = async () => {
-    console.log("iniciando jogo...");
     setIsLoadingNewGame(true);
     const result = await startGame();
     setIsLoadingNewGame(false);
+    setAttempts(0);
+    setGuessHistory([]);
+    setIsGameOver(false);
     setMessage("Novo jogo iniciado! Faça seu palpite.");
   };
 
   // Função para enviar um palpite
   const handleMakeGuess = async () => {
-    console.log(`Enviando palpite: ${guess}`);
     if (!guess) {
       setMessage('Por favor, insira um número entre 1 e 100.');
       return;
@@ -28,7 +32,10 @@ function App() {
     setIsLoadingGuess(true);
     const result = await makeGuess(guess);
     setIsLoadingGuess(false);
-    setMessage(result);
+    setMessage(result.message);
+    setAttempts(result.attempts);
+    setIsGameOver(result.isGameOver);
+    setGuessHistory([...guessHistory, guess]);
     setGuess('');
   };
 
@@ -46,11 +53,19 @@ function App() {
       </button>
       <br /> <br />
       <div>
-        <input type="number" value={guess} onChange={(e) => setGuess(e.target.value)} placeholder='Digite seu palpite' />
+        <input type="number" value={guess} onChange={(e) => setGuess(e.target.value)} 
+          placeholder='Digite seu palpite' disabled={isGameOver || isLoadingGuess} />
         <br /> <br />
-        <button onClick={handleMakeGuess} disabled={isLoadingGuess}>
+        <button onClick={handleMakeGuess} disabled={isGameOver || isLoadingGuess}>
           {isLoadingGuess ? 'Enviando...' : 'Enviar Palpite'}
         </button>
+        {/* Só mostra essa seção se o jogo já tiver começado */}
+        {attempts > 0 && (
+          <div className="game-info">
+            <p>Tentativas: {attempts}</p>
+            <p>Palpites já feitos: {guessHistory.join(', ')}</p>
+          </div>
+        )}
       </div>
       <br /> <hr /> <br />
       </div>
